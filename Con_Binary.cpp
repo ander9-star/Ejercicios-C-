721d28c71ae0c6bae2f3b0b95c79338fe00e0314
andervilla09 <109714460+ander9-star@users.noreply.github.com>
2023-04-15 23:55:16 -0500

    Add files via upload

---
+++ Con_Binary.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<Windows.h>
+
+using namespace std;
+
+void Convertidor_Binario(int decimal);
+
+int main() {
+
+	int decimal;
+
+	cout << "Ingrese un numero: "; cin >> decimal;
+
+	Convertidor_Binario(decimal);
+
+	return 0;
+}
+
+//Funcion para convertir el numero a decimal
+void Convertidor_Binario (int decimal) {
+
+	int guardar = decimal;
+	int residuo = 0;
+	vector<int> binary;
+
+	while (decimal != 1) {
+		residuo = (int) fmod(decimal, 2);
+		decimal = decimal / 2;
+		binary.push_back(residuo);
+		residuo++;
+	}
+	binary.push_back(decimal); 
+
+	system("cls");
+	cout << guardar << " en Binario es: ";
+	Sleep(100);
+	for (auto it = binary.rbegin(); it != binary.rend(); ++it) {
+		cout << *it;
+	}
+	cout << endl;
+}
+
